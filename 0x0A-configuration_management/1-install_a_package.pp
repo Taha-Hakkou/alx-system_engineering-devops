355baba12e766dc1ac7cea594c9ed8a3591133a8
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-02-23 22:40:12 +0100

    0x0A cm corr4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,4 @@
 # installs flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  require  => 'python-3.8.10',
-  provider => 'pip3',
+exec { 'flask':
+  command => 'pip3 install flask -v 2.1.0',
 }
