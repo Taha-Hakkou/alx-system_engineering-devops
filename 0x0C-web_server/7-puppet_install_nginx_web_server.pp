0be534867e801c64acdfa0e77fb816050baf5c7b
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-02-28 18:47:27 +0100

    0x0C corr16

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,19 @@
 # installs and configures an Nginx server & performs a 301 redirect when querying /redirect_me
 exec { 'update_packages':
-  command => 'apt-get update',
+  command => '/usr/bin/apt-get -y update && /usr/bin/apt-get -y upgrade',
 }
 
 package { 'nginx':
-  ensure  => installed,
+  ensure   => installed,
+  provider => 'apt',
 }
 
-file { '/var/www/html/index.nginx-debian.html':
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
 exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  command  => '/usr/bin/sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default',
 }
 
 service { 'nginx':
