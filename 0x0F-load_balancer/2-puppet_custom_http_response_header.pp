823a491dbfe3ff7bc6ba3deb4c683bcbedbed28e
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-03-04 15:35:29 +0100

    0x0F load_balancer done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# installs and configures an HAProxy server with custom "X-Served-By" HTTP header
+exec { 'update_packages':
+  command => '/usr/bin/apt-get -y update && /usr/bin/apt-get -y upgrade',
+}
+
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt',
+}
+
+exec {'header':
+  command  => '/usr/bin/sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By $hostname;/" /etc/nginx/sites-available/default',
+}
+
+service { 'nginx':
+  ensure  => running,
+}
---
+++ 0x0F-load_balancer/README.md
@@ -0,0 +1,7 @@
+# 0x0F-load_balancer
+
+Load Balancer Project
+
+## Tasks:
+
+0-custom_http_response_header
