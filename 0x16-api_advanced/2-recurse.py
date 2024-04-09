e47cd18a0679e594840b4cb37966d62b8ace7a9d
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-04-09 23:31:01 +0000

    0x16 api_advanced corr5

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,32 +7,27 @@ def recurse(subreddit, hot_list=[], after='', count=0):
     """returns a list containing the titles
     of all hot articles for a given subreddit"""
     """if subreddit is None or not isinstance(subreddit, str):
-        return (None)
+        return (None)"""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    response = requests.get(url, params={'after': after},
+    response = requests.get(url, params={'count': count, 'after': after},
                             headers={'User-Agent': 'MyAPI/0.0.1'},
                             allow_redirects=False)
-    posts = response.json().get('data', {}).get('children', [])
+    """posts = response.json().get('data', {}).get('children', [])
     titles = [post.get('data').get('title') for post in posts]
     hot_list += titles
     after = response.json().get('data', {}).get('after', None)
     if after:
         hot_list = recurse(subreddit, hot_list, after)
     return (hot_list)"""
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"count": count, "after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 400:
+    if response.status_code >= 400:
         return None
 
     hot_l = hot_list + [child.get("data").get("title")
-                        for child in sub_info.json()
+                        for child in response.json()
                         .get("data")
                         .get("children")]
 
-    info = sub_info.json()
+    info = response.json()
     if not info.get("data").get("after"):
         return hot_l
 
